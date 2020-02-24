9887df80804ff3ed66a46a99e8c3af0106648366
Andrew Thomas <athomik79@gmail.com>
2020-02-24 15:46:16 -0500

    added event listen for password generator function

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+// Dom elements
+var resultEl = document.getElementById("result");
+var lengthEl = document.getElementById("length");
+var uppercaseEl = document.getElementById("uppercase");
+var lowercaseEl = document.getElementById("lowercase");
+var numberEl = document.getElementById("number");
+var symbolEl = document.getElementById("symbol");
+var generateEl = document.getElementById("generate");
+var clipboardEl = document.getElementById("clipboard");
+
 // Add functions to object randomFunc
 var randomFunc = {
-    lower: getRandomLower,
     upper: getRandomUpper,
+    lower: getRandomLower,
     number: getRandomNumber,
     symbol: getRandomSymbol
 };
+
+// Generate event listen
+generateEl.addEventListener("click", () => {
+    var length = +lengthEl.value;
+    var hasUpper = uppercaseEl.checked;
+    var hasLower = lowercaseEl.checked;
+    var hasNumber = numberEl.checked;
+    var hasSymbol = symbolEl.checked;
+
+    // console.log(hasLower, hasUpper, hasNumber, hasSymbol)
+    
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
 
 // Generator functions
 // from charCode method on String object
@@ -26,4 +49,4 @@ function getRandomLower() {
   return symbols[Math.floor(Math.random() * symbols.length)];
  }
 
-//  console.log(getRandomLower());
+    //  console.log(getRandomLower());
