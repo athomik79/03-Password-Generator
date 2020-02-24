089844242ece69c68d4272be9e97fefa0d1d29f5
Andrew Thomas <athomik79@gmail.com>
2020-02-24 17:29:40 -0500

    added generate password function: initiate password variable, filter out uncheched types, loop over length call generator function for each type, add final password to password variabl and return

--- script.js
+++ script.js
@@ -29,6 +29,47 @@ generateEl.addEventListener("click", () => {
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
+// Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+    // 1. Init pw var
+    // 2. Filter out unchecked types
+    // 3. Loop over length call generator function for each type
+    // 4. Add final pw to the pw var and return
+
+    let generatePassword = "";
+
+    var typesCount =  upper + lower + number + symbol;
+
+    // console.log("typesCount: ", typesCount);
+
+    var typesArr = [{upper}, {lower}, {number}, {symbol}].filter
+    (
+        item => Object.values(item)[0]
+    );
+
+    // console.log("typesArr: ", typesArr);
+
+    if(typesCount === 0) {
+        return "";
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            var funcName = Object.keys(type)[0];
+
+        // console.log("funcName: ", funcName);
+
+            generatePassword += randomFunc[funcName]();
+          });
+        }
+
+        // console.log(generatePassword);
+
+        var finalPassword = generatePassword.slice(0, length);
+
+        return finalPassword;
+    }
+
 // Generator functions
 // from charCode method on String object
 
